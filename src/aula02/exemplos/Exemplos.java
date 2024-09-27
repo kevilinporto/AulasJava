07053a246c97df28d78b9508eea12dc4a24d3d1e
Miguel <buguelo246@gmail.com>
2024-09-27 09:29:52 -0300

    aula 27/09/2024 tipos de dados

---
+++ src/aula02/exemplos/Exemplos.java
@@ -0,0 +1,19 @@
+package aula02.exemplos;
+
+public class Exemplos {
+    public static void main(String[] args) {
+        int anoNasc;
+        double peso;
+        char sexo;
+        boolean canhoto;
+
+          anoNasc = 2006;
+          peso = 61.0;
+          sexo = 'F';
+
+          anoNasc++;
+
+        System.out.println(anoNasc);
+
+    }
+}
--- src/aula02/exercicios/Exercicio01.java
+++ src/aula02/exercicios/Exercicio01.java
@@ -3,5 +3,13 @@ package aula02.exercicios;
 public class Exercicio01 {
     public static void main(String[] args) {
 
+        int idade = 18;
+        float altura = 1.67f;
+        double peso = 61.4;
+
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(peso);
+
     }
 }
