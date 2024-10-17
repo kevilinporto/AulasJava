5c216e7227b052ab979a0c9e2d86cf7e422e8fe1
Kevilin Porto <kevilinvitoria22@gmail.com>
2024-10-17 09:25:06 -0300

    atualização 17/10/2024

--- src/aula02/exercicios/Exercício07.java
+++ src/aula02/exercicios/Exercício07.java
@@ -7,13 +7,26 @@ public class Exercício07 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite um número: ");
+        System.out.println("Digite o primeiro número: ");
         int var1 = scanner.nextInt();
 
-        System.out.println("Digite outro número: ");
+        System.out.println("Digite o segundo número: ");
         int var2 = scanner.nextInt();
 
-        boolean cond1 = var1 >= var2 &&
+        System.out.println("Digite o terceiro número: ");
+        int var3 = scanner.nextInt();
+
+        System.out.println("Digite o quarto número: ");
+        int var4 = scanner.nextInt();
+
+        boolean cond1;
+        boolean cond2;
+        boolean cond3;
+
+        cond1 = var1 > var2 && var3 > var4;
+        System.out.println(var1 + " > " + var2 + " && " + var3 + " > " + var4 + " = " + cond1);
+
+        scanner.close();
 
 
 
